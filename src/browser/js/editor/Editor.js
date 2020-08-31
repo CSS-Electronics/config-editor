90ae3a744625b94f60b66ba09b2057d5021ffd58
mfcss <mf@csselectronics.com>
2020-08-31 13:47:25 +0200

    Updated config editor to use latest config-editor-base

--- src/browser/js/editor/Editor.js
+++ src/browser/js/editor/Editor.js
@@ -12,6 +12,20 @@ import {EditorSection} from "config-editor-base";
 import * as actionsAlert from "../alert/actions";
 import AlertContainer from "../alert/AlertContainer";
 
+// define UIschema and Rule Schema names for auto-loading purposes
+export const uiSchemaAry = [
+  "uischema-01.02.json | Simple",
+  "uischema-01.02.json | Advanced",
+];
+
+export const schemaAry = [
+  "schema-01.02.json | CANedge2",
+  "schema-01.02.json | CANedge1",
+  "schema-00.07.json | CANedge2",
+  "schema-00.07.json | CANedge1",
+];
+
+
 class Editor extends React.Component {
   render() {
     let editorTools = [
@@ -36,13 +50,17 @@ class Editor extends React.Component {
     ];
 
     return (
-      <React.Fragment>
+       <div className="file-explorer">
+	   <div className="fe-body fe-body-offline" >
         <AlertContainer />
         <EditorSection
           editorTools={editorTools}
           showAlert={this.props.showAlert}
+		  uiSchemaAry={uiSchemaAry}
+          schemaAry={schemaAry}
         />
-      </React.Fragment>
+		</div>
+      </div>
     );
   }
 }
