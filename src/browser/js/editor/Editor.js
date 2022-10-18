337076e12212a31258a242a96cc710d927d2d6b3
mfcss <mf@csselectronics.com>
2022-10-18 13:33:21 +0200

    Misc CSS edits and updates to imported packages

--- src/browser/js/editor/Editor.js
+++ src/browser/js/editor/Editor.js
@@ -14,7 +14,7 @@ import AlertContainer from "../alert/AlertContainer";
 
 // define editor title and version
 const title = TYPE + " config editor"
-const version = "v1.4.8"
+const version = "v1.6.6"
 
 // define UIschema and Rule Schema names for auto-loading purposes
 export const uiSchemaAry = {"CANedge": [
